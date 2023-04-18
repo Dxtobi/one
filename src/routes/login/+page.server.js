8244f2ae5b9fdf226fceb925eb65d18e412ae911
dxtobi <akanbijosephtobi@gmail>
2023-04-18 04:56:09 -0700

    added login functions..

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -5,10 +5,6 @@ import { error, redirect } from '@sveltejs/kit';
 export const actions = {
     login: async ({locals, request}) =>{
         const body = Object.fromEntries(await request.formData());
-
-        console.log(locals.pb)
-
-        //console.log(body, username)
         try {
             await locals.pb.collection('users').authWithPassword(body.email, body.password); 
             if (!locals.pb?.authStore?.model?.verified) {
@@ -18,7 +14,7 @@ export const actions = {
                 }
             }
         } catch (err) {
-            console.log('ERRR FROM REGISTER SERVER:::', err);
+            
             throw error(500, 'something went wrong')
         }
 
